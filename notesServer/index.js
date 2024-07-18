1ba8bbfa939c617f0b979f4c5b3fb8a1f3b60923
Alexander Eklöw <eklow.alexander@gmail.com>
2024-07-18 20:36:18 +0200

    expanded  example app

--- notesServer/index.js
+++ notesServer/index.js
@@ -17,6 +17,19 @@ const generateId = () => {
   return String(maxId + 1);
 };
 
+const requestLogger = (request, _, next) => {
+  console.log("Method: ", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(requestLogger);
 app.use(express.json());
 
 app.get("/", (request, response) => {
@@ -60,6 +73,8 @@ app.post("/api/notes", (request, response) => {
   console.log(note);
   response.json(note);
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
